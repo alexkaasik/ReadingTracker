9dcc98873d6ef38fbe97ad4c2757c82f9865a3a8
alexkaasik <aleks.kaasik@outlook.com>
2025-01-14 17:21:39 +0200

    Added some more columns of info the tableview.

--- FrontEnd/src/components/BooksTable.vue
+++ FrontEnd/src/components/BooksTable.vue
@@ -12,15 +12,25 @@ export default {
         <table class="table table-striped table-bordered">
             <thead class="table-dark">
                 <tr>
-                <th>Books Name</th>
-                <th>Description</th>
-            </tr>
+                    <th>Books Name</th>
+                    <th>Description</th>
+                    <th>Gerne</th>
+                    <th>ReleaseDate </th>
+                    <th>ReviewScore</th>
+                    <th>Length</th>
+                </tr>
             </thead>
             <tbody>
                 <tr v-for="item in items" :key="item">
                     
                     <td>{{ item.BookName }}</td>
-                    <td>{{ item.Description }}</td>                    
+                    <td>{{ item.Description }}</td>
+                    <td>
+                        <div v-for="Gerne in item.Gerne" :key="Gerne">{{ Gerne }},</div>
+                    </td>
+                    <td>{{ item.ReleaseDate  }}</td>
+                    <td>{{ item.ReviewScore }}</td>
+                    <td>{{ item.HowManyPages }} pages</td>                    
                 </tr>
             </tbody>
             
