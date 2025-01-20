c85b629d2ff4919c4ac2dc34fdfca421d717bb46
mihutka <misabajandinov@gmail.com>
2025-01-20 11:26:40 +0200

    Make DeleteFrontEnd
    Make DeleteFrontEnd

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/books">Books</RouterLink>
+        
       </nav>
     </div>
   </header>
---
+++ FrontEnd/src/views/DeleteView.vue
@@ -0,0 +1,23 @@
+<script>
+import BooksTable from '../components/BooksTable.vue';
+export default{
+    components: { BooksTable },
+    data() {
+        return {
+            allBooks: []
+        }
+    },
+    async delete() {
+        this.allBooks = await (await fetch('http://localhost:8080/books')).json();
+        //console.log("called from gamesview");  // for debugging purposes
+    }
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style scoped></style>
