299b812022cddca9e0d3d55e468305447d42df5a
alexkaasik <aleks.kaasik@outlook.com>
2025-01-21 13:30:11 +0200

    Added basic detail page to FrontEnd

---
+++ FrontEnd/src/views/BooksDetail.vue
@@ -0,0 +1,23 @@
+<script>
+import BooksDetailTable from '../components/BooksDetailTable.vue';
+export default{
+    components: { BooksDetailTable },
+    data() {
+        return {
+            oneBook: {}
+        }
+    },
+    async created() {
+        this.oneBook = await (await fetch(`http://localhost:8080/books/${this.route.id}`)).json();
+        //console.log("called from gamesview");  // for debugging purposes
+    }
+}
+</script>
+
+<template>
+    <main>
+        <BooksDetailTable :item=oneBook />
+    </main>
+</template>
+
+<style scoped></style>
