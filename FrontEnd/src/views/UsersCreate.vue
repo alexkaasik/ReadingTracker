fa33e2e2f11ffa4b902c468f35ca7d4b7c4e20da
mihutka <misabajandinov@gmail.com>
2025-01-30 01:48:39 +0200

    Add UsersdCreate.vue

---
+++ FrontEnd/src/views/UsersCreate.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Create New User</h2>
+    <p>Форма для создания пользователя</p>
+  </div>
+</template>
---
+++ FrontEnd/src/views/UsersUpdate.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Update User</h2>
+    <p>Форма для обновления пользователя (ID: {{ $route.params.id }})</p>
+  </div>
+</template>
