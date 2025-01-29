8e09ac5aca9ce1de20e188ac6da51caa0eeced70
mihutka <misabajandinov@gmail.com>
2025-01-30 01:12:21 +0200

    Add UserView.vue

---
+++ FrontEnd/src/views/UsersView.vue
@@ -0,0 +1,27 @@
+<script>
+import UsersTable from "@/components/UsersTable.vue";
+
+export default {
+  components: { UsersTable },
+  data() {
+    return {
+      users: []
+    };
+  },
+  async created() {
+    try {
+      const response = await fetch("http://localhost:8080/users");
+      this.users = await response.json();
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h2>Users List</h2>
+    <UsersTable :users="users" />
+  </div>
+</template>
