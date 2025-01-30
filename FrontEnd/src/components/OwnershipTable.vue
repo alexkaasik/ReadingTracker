aff1d89dbf256a28811c93804d0053c620185406
mihutka <misabajandinov@gmail.com>
2025-01-30 02:07:51 +0200

    Create OwnershipTable.vue

---
+++ FrontEnd/src/components/OwnershipTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Owner ID</th>
+          <th>Book ID</th>
+          <th>User ID</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ownership in ownerships" :key="ownership.OwnerId">
+          <td>{{ ownership.OwnerId }}</td>
+          <td>{{ ownership.BookId }}</td>
+          <td>{{ ownership.UserId }}</td>
+          <td>{{ ownership.Status }}</td>
+          <td>
+            <router-link :to="'/ownerships/' + ownership.OwnerId">Detail</router-link> |
+            <router-link :to="'/ownerships/update/' + ownership.OwnerId">Update</router-link> |
+            <button @click="$emit('delete-ownership', ownership.OwnerId)" class="delete-btn">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <router-link to="/ownerships/create" class="create-btn">Create</router-link>
+  </div>
+</template>
