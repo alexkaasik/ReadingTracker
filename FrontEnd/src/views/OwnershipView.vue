eeaa13e4f846a084ef00394df92869cc4d68d2e6
mihutka <misabajandinov@gmail.com>
2025-01-30 02:17:33 +0200

    Create OwnershipView.vue

---
+++ FrontEnd/src/views/OwnershipView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>Ownership List</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>OwnerId</th>
+          <th>BookId</th>
+          <th>UserId</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ownership in ownerships" :key="ownership.OwnerId">
+          <td>{{ ownership.OwnerId }}</td>
+          <td>{{ ownership.BookId }}</td>
+          <td>{{ ownership.UserId }}</td>
+          <td>{{ ownership.Status }}</td>
+          <td>
+            <router-link :to="`/ownerships/${ownership.OwnerId}`">Detail</router-link> |
+            <router-link :to="`/ownerships/update/${ownership.OwnerId}`">Update</router-link> |
+            <button @click="deleteOwnership(ownership.OwnerId)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <router-link to="/ownerships/create">Create New Ownership</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      ownerships: []
+    };
+  },
+  created() {
+    this.fetchOwnerships();
+  },
+  methods: {
+    fetchOwnerships() {
+      axios.get("http://localhost:8080/ownerships")
+        .then(response => {
+          this.ownerships = response.data;
+        })
+        .catch(error => {
+          console.error("Error fetching ownerships:", error);
+        });
+    },
+    deleteOwnership(ownerId) {
+      axios.delete(`http://localhost:8080/ownerships/${ownerId}`)
+        .then(() => {
+          this.ownerships = this.ownerships.filter(o => o.OwnerId !== ownerId);
+        })
+        .catch(error => {
+          console.error("Error deleting ownership:", error);
+        });
+    }
+  }
+};
+</script>
