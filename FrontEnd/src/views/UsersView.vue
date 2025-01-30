27c009ee7fdb6aa4eeb42f7fc964ad4197ed5d24
mihutka <misabajandinov@gmail.com>
2025-01-30 02:35:30 +0200

    update

--- FrontEnd/src/views/UsersView.vue
+++ FrontEnd/src/views/UsersView.vue
@@ -1,38 +1,80 @@
-<script>
-import UsersTable from "@/components/UsersTable.vue";
-
-export default {
-  components: { UsersTable },
-  data() {
-    return {
-      users: [],
-    };
-  },
-  async created() {
-    try {
-      const response = await fetch("http://localhost:8080/users");
-      this.users = await response.json();
-    } catch (error) {
-      console.error("Failed to fetch users:", error);
-    }
-  },
-  methods: {
-    async deleteUser(userId) {
-      try {
-        await fetch(`http://localhost:8080/users/${userId}`, { method: "DELETE" });
-        this.users = this.users.filter(user => user.UserId !== userId);
-      } catch (error) {
-        console.error("Failed to delete user:", error);
+<template>
+    <div>
+      <h1>Users List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Ownership ID</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.UserId">
+            <td>{{ user.UserId }}</td>
+            <td>{{ user.FirstName }}</td>
+            <td>{{ user.LastName }}</td>
+            <td>{{ user.UserName }}</td>
+            <td>{{ user.OwnerShipListID }}</td>
+            <td>
+              <router-link :to="`/users/${user.UserId}`">Detail</router-link>
+              <router-link :to="`/users/update/${user.UserId}`">Update</router-link>
+              <button @click="deleteUser(user.UserId)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <router-link to="/users/create">Create</router-link>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    created() {
+      this.fetchUsers();
+    },
+    methods: {
+      fetchUsers() {
+        axios.get("http://localhost:8080/users")
+          .then(response => {
+            this.users = response.data;
+          })
+          .catch(error => {
+            console.error("Error fetching users:", error);
+          });
+      },
+      deleteUser(userId) {
+        axios.delete(`http://localhost:8080/users/${userId}`)
+          .then(() => {
+            this.fetchUsers(); 
+          })
+          .catch(error => {
+            console.error("Error deleting user:", error);
+          });
       }
     }
+  };
+  </script>
+  
+  <style scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
   }
-};
-</script>
-
-<template>
-  <div>
-    <h2>Users List</h2>
-    <UsersTable :users="users" @delete-user="deleteUser" />
-  </div>
-</template>
+  th, td {
+    padding: 8px;
+    border: 1px solid #ddd;
+  }
+  </style>
+  
 
